1de456924292116daac0774c4b7829dfb592138f
Andrea Francesco Iuorio <andreafrancesco.iuorio@gmail.com>
2012-12-18 12:45:15 +0100

    Implemented bsearch function.

--- panC-Lib/src/panC-Lib.c
+++ panC-Lib/src/panC-Lib.c
@@ -1,42 +1,24 @@
 
 #include <stdio.h>
 #include "stdlib.h"
+
+int compareInt (const void * a, const void * b)
+{
+  return ( *(int*)a - *(int*)b );
+}
+
 int main(void) {
-	testAbsLabs();
+	int array[500000];
+	register int i;
+	for(i=0; i < 500000; i++)
+		array[i] = i;
+
+	for(i=0; i < 500000; i++) {
+		int zero = i;
+		int *isZero = bsearch(&zero,array, 500000, sizeof(int), compareInt) ;
+		printf("%d\n",*isZero);
+	}
 	return 0;
 }
 
-void testRandom()
-{
-	srand(10);
-	register int i = 0;
-	int numError = 0;
-	char mapControl[32768];
 
-	for ( i = 0; i < 32768; i++)
-		mapControl[i] = 0;
-	for ( i = 0; i < 32768; i++){
-			int actual = rand();
-			if(mapControl[actual] != 0){
-				printf("Error at %d\n", actual);
-				numError++;
-			}
-			else
-				mapControl[actual] = 1;
-	}
-	printf("NumError = %d\n",numError);
-}
-
-void testAbsLabs(){
-	signed int intA = 3;
-	signed long longA= 13;
-
-	intA = abs(intA);
-	longA = labs(longA);
-	printf("%d %d\n",intA,longA);
-	intA = abs(intA);
-	longA = labs(longA);
-	printf("%d %d\n",intA,longA);
-
-
-}
--- panC-Lib/stdlib/stdlib.h
+++ panC-Lib/stdlib/stdlib.h
@@ -10,11 +10,13 @@
 
 #define RAND_MAX 32768
 
+typedef unsigned int size_t;
+
 void srand(unsigned int seed);
 int rand();
 int labs(long n);
 int abs(int n);
-
+void *bsearch(const void *key, const void *base, size_t n, size_t size, int(*cmp)(const void *keyval,const void *datum) );
 
 
 #endif /* STDLIB_H_ */
